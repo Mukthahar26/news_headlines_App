c1e302de06118fbfb40bb1f60a2ffd7046920585
Mukthahar26 <mukthaharshaik@gmail.com>
2023-12-22 16:36:52 +0530

    removed some contants

--- src/themes/sizeValues.ts
+++ src/themes/sizeValues.ts
@@ -3,10 +3,8 @@ import {Dimensions} from 'react-native';
 
 const {height, width} = Dimensions.get('screen');
 const sizeValues = {
-  fontSmall: scale(10),
   fontNormal: scale(12),
   fontMedium: scale(14),
-  fontLarge: scale(16),
   size16: scale(16),
   size10: scale(10),
   screenHeight: height,
